c7e6b21030950f230014ed4d0a6df32b06ad8070
uv-07 <63532070+uv-07@users.noreply.github.com>
2022-11-12 17:02:55 +0530

    last

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 console.log("Welcome To Spotify");
 
 let songIndex = 0;
-let audioElement = new Audio('AHP.mp3');
+let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('master-Play');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
-
+let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    { songName: "Aarambh Hai Prachand", filePath: "song/AHP.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Aarambh Hai Prachand", filePath: "song/AHP.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Aarambh Hai Prachand", filePath: "song/AHP.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Aarambh Hai Prachand", filePath: "song/AHP.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Aarambh Hai Prachand", filePath: "song/AHP.mp3", coverPath: "covers/1.jpg" },
+    { songName: "StarBoy - Daft punk", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+    { songName: "Tera Ban Jaunga", filePath: "songs/2.mp3", coverPath: "covers/2.jpg" },
+    { songName: "Tujhe Kitna Chahne lage Hum", filePath: "songs/3.mp3", coverPath: "covers/3.jpg" },
+    { songName: "Kaise Hua", filePath: "songs/4.mp3", coverPath: "covers/4.jpg" },
+    { songName: "Teri Khair Mangdi", filePath: "songs/5.mp3", coverPath: "covers/5.jpg" },
 ]
+
+songItems.forEach((element, i) => {
+    console.log(element, i);
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+})
 
 // audioElement.play();
 
@@ -40,4 +46,26 @@ audioElement.addEventListener('timeupdate', () => {
 
 myProgressBar.addEventListener("change", () => {
     audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
+})
+
+const makeAllPlay = () => {
+
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+        element.classList.remove('fa-pause-circle')
+        element.classList.add('fa-play-circle')
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+    element.addEventListener('click', (e) => {
+        makeAllPlay();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle')
+        e.target.classList.add('fa-pause-circle')
+        audioElement.src = `songs/${songIndex}.mp3`;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
 })
